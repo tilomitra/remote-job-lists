7c4d00a88fc3d4870c3c2333095a6f8c7fb50dfd
Tilo Mitra <tilomitra@gmail.com>
2018-03-12 20:18:01 -0400

    update server config for prod

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const bodyParser = require('body-parser')
 const api = require('./routes/api');
 
 const dev = process.env.NODE_ENV !== 'production'
+const port = process.env.PORT || 3000;
 const app = next({ dev })
 const handle = app.getRequestHandler()
 
@@ -52,9 +53,9 @@ app.prepare()
             return handle(req, res)
         })
 
-        server.listen(3000, (err) => {
+        server.listen(port, (err) => {
             if (err) throw err
-            console.log('> Ready on http://localhost:3000')
+            console.log(`> Ready on port:${port} with NODE_ENV:${process.env.NODE_ENV}`)
         })
     })
     .catch((ex) => {
